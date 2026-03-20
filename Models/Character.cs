e8cb52e45f5bfc65095b4c6667c58f84dc904de7
jhon <jyate@smartsoft.la>
2026-03-19 22:38:09 -0500

    Adds `Character` model for representing video game characters.

---
+++ Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoGameCharactersApi.Models;
+
+public class Character
+{ 
+    public string Id { get; set; }
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public string Game { get; set; } = string.Empty;
+    
+    public string Role { get; set; } = string.Empty;
+    
+} 
