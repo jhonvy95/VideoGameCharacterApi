06ad440b858674ed1528a07ab930c17704604f18
jhon <jyate@smartsoft.la>
2026-03-19 22:38:52 -0500

    Adds `IVideoGameCharacterService` interface with CRUD method definitions

---
+++ Services/IVideoGameCharacterService.cs
@@ -0,0 +1,12 @@
+﻿using VideoGameCharactersApi.Models;
+
+namespace VideoGameCharactersApi.Services;
+
+public interface IVideoGameCharacterService
+{
+    Task<List<Character>> GetAllCharactersAsync();
+    Task<Character> GetCharacterByIdAsync(int id);
+    Task<Character> CreateCharacterAsync(Character character);
+    Task<bool> UpdateCharacterAsync(int id, Character character);
+    Task<bool> DeleteCharacterAsync(int id);
+}
