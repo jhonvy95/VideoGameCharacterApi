208e77d9edecad4b24725c8f38a5c1d11216746b
jhon <jyate@smartsoft.la>
2026-03-29 19:11:03 -0500

    Configures Entity Framework Core with SQL Server and updates `Character.Id` to use `int`.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharactersApi.Models;
+
+namespace VideoGameCharactersApi.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+{
+    public DbSet<Character> Characters => Set<Character>();
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -2,7 +2,7 @@
 
 public class Character
 { 
-    public string Id { get; set; }
+    public int Id { get; set; }
     
     public string Name { get; set; } = string.Empty;
     
